95abf96ce0829b04468976d204b7ddb1428e5d48
Faisal Ahammad <faisalahammad24@gmail.com>
2021-08-25 17:32:55 +0600

    Added - Word count functionality

--- word-count.php
+++ word-count.php
@@ -29,3 +29,18 @@
     load_plugin_textdomain( 'wordcount', false,  dirname(__FILE__).'/languages');
   }
   add_action( 'plugins_loaded', 'wordcount_load_textdomain' );
+
+  // Count function
+  function wordcount_count_words($content) {
+    // strip / remove html tags from post
+    $stripped_content = strip_tags($content);
+    // count word
+    $word_count = str_word_count($stripped_content);
+    // Label / Displayed text
+    $label_text = __('Total number of words', 'wordcount');
+    // show the content
+    $content .= sprintf('<h3>%s: %s</h3>', $label_text, $word_count);
+    // return content
+    return $content;
+  }
+  add_filter( 'the_content', 'wordcount_count_words');
